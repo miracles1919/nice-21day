ca39db24a94ea3d2a5d5c9e590a0c374b1052287
huangquanya <1036871949@qq.com>
2022-07-26 15:11:18 +0800

    fix: 添加管理员页编辑和删除操作

--- packages/nice-21day-shared/src/types/admin.ts
+++ packages/nice-21day-shared/src/types/admin.ts
@@ -1,14 +1,14 @@
 import { EState } from './global';
 
 export interface IAdmin {
-  id: string;
+  id?: string;
   login_name: string;
   nick_name: string;
   /** 查询时不会返回密码 */
   password?: string;
   state: EState;
-  deleted: boolean;
-  created_at: string;
+  deleted?: boolean;
+  created_at?: string;
   updated_at?: string;
   deleted_at?: boolean;
 }
--- packages/nice-21day-web/src/pages/Admin/index.tsx
+++ packages/nice-21day-web/src/pages/Admin/index.tsx
@@ -1,12 +1,20 @@
-import { queryAdminsList } from '@/services';
+import { queryAdminsList, deleteAdmin } from '@/services';
 import { PlusOutlined } from '@ant-design/icons';
-import { ProColumns, ProTable } from '@ant-design/pro-components';
+import { ProColumns, ProTable, ActionType } from '@ant-design/pro-components';
 import { IAdmin } from '@nice-21day/shared';
 import { history } from '@umijs/max';
-import { Button, Space } from 'antd';
-import React from 'react';
+import { Button, Space, Modal } from 'antd';
+import React, { useRef, useState } from 'react';
 
 const Admin: React.FC = () => {
+  const tableRef = useRef<ActionType>();
+  const [curListLen, setCurListLen] = useState(0)
+
+  const handleDeletAdmin = (id: string) => {
+    deleteAdmin(id).then(() => {
+      tableRef.current?.reload()
+    })
+  }
   const columns: ProColumns<IAdmin>[] = [
     {
       title: '登录名',
@@ -28,23 +36,37 @@ const Admin: React.FC = () => {
     {
       title: '操作',
       dataIndex: 'action',
-      render: () => (
+      render: (_, item) => (
         <Space>
-          <span>编辑</span>
-          <span>删除</span>
+          <span className='ant-btn ant-btn-link' onClick={() => history.push(`/admin/update/${item.id || ''}`)}>编辑</span>
+          {
+          curListLen !== 1
+            &&(<span className='ant-btn ant-btn-link' onClick={() =>{
+            const { id = '' } = item;
+            Modal.confirm({
+              title: `确定要删除管理员${item.nick_name}吗？`,
+              onOk: () => {
+                handleDeletAdmin(id)
+              },
+            });
+          }}>删除</span>)
+          }
         </Space>
       ),
     },
   ];
 
+
   return (
     <ProTable<IAdmin>
+      actionRef={tableRef}
       rowKey="id"
       size="small"
       bordered
       pagination={false}
       request={async () => {
         const admins = await queryAdminsList();
+        setCurListLen(admins.length)
         return {
           success: true,
           data: admins,
